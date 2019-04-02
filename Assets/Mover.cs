0966a3c0164d34bad8f9cbe12d56b95414915709
droune2001 <nfauvet@gmail.com>
2019-04-02 11:00:41 +0200

    Initial commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+
+    NavMeshAgent nm = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target)
+        {
+            nm.destination = target.position;
+        }
+    }
+}
